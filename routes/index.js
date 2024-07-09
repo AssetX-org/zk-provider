ab556cb64c760f917f42b6c2e40e1278982bdb19
Ankit Kokane <ankitkokane90@gmail.com>
2024-07-09 06:06:45 +0530

    added zk proof gen logic and API endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
+import express from "express";
+import { ProofGen } from "../utils/zokrates.js";
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+router.post("/", async (req, res) => {
+  try {
+    const { owner, title, data } = req.body;
+
+    console.log(owner, title, data);
+    console.log("body:", req.body);
+    const proof = await ProofGen(owner, title, data);
+    console.log(proof);
+    res.json({ proof });
+  } catch (error) {
+    console.error("Error generating proof:", error);
+    res.status(500).json({ error: "Failed to generate proof" });
+  }
 });
 
-module.exports = router;
+export default router;
---
+++ utils/zokrates.js
@@ -0,0 +1,78 @@
+import * as crypto from "crypto";
+import { initialize } from "zokrates-js";
+import { readFileSync } from "fs";
+
+function hashString(inputString, hashLength) {
+  const hash = crypto.createHash("sha256");
+  hash.update(inputString);
+  return hash.digest("hex").slice(0, hashLength);
+}
+
+function stringToHex(str) {
+  let hex = "";
+  for (let i = 0; i < str.length; i++) {
+    let charCode = str.charCodeAt(i).toString(16);
+    hex += charCode.length < 2 ? "0" + charCode : charCode;
+  }
+  return hex;
+}
+
+let zk = null;
+let artifacts = null;
+let provingKey = null;
+
+function base64ToUint8Array(base64String) {
+  const binaryString = atob(base64String);
+  const length = binaryString.length;
+  const uint8Array = new Uint8Array(length);
+  for (let i = 0; i < length; i++) {
+    uint8Array[i] = binaryString.charCodeAt(i);
+  }
+  return uint8Array;
+}
+
+async function initializeZok() {
+  if (!zk) {
+    zk = await initialize();
+
+    try {
+      const source = readFileSync("zk-circuit/root.zok", "utf-8");
+      artifacts = zk.compile(source);
+
+      provingKey = base64ToUint8Array(
+        readFileSync("zk-circuit/proving.key").toString("base64")
+      );
+
+      console.log("Initialized Zokrates.");
+    } catch (error) {
+      console.error("Error initializing Zokrates:", error);
+      zk = null;
+    }
+  }
+}
+
+export async function ProofGen(owner, title, data) {
+  try {
+    console.log("Initializing Zokrates...");
+    await initializeZok();
+
+    const hashedOwner = stringToHex(hashString(owner, 16));
+    const hashedTitle = stringToHex(hashString(title, 16));
+    const hashedData = stringToHex(hashString(data || "", 16));
+
+    const out = zk.computeWitness(artifacts, [
+      hashedOwner,
+      hashedTitle,
+      hashedData,
+    ]);
+    console.log("Computed witness:", out.witness);
+    console.log("artifact:", artifacts.program);
+    console.log("provingKey:", provingKey);
+    const proof = zk.generateProof(artifacts.program, out.witness, provingKey);
+
+    return proof;
+  } catch (error) {
+    console.error("Error processing request:", error);
+    throw error;
+  }
+}
